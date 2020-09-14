8720b83dad2b3d9db13b5c0fb291bae96e2f6331
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-09-14 12:46:05 +0700

    Minggu 3

---
+++ VR Pertemuan Ketiga/Assets/UbahWarna.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarna : MonoBehaviour
+{
+    public void Hijau()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void Merah()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Biru()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
