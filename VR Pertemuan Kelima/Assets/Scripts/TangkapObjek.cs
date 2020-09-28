4d296f59dd3f226b27ea697d16a115b37bf01fc7
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-09-28 11:59:44 +0700

    Minggu 5

---
+++ VR Pertemuan Kelima/Assets/Scripts/TangkapObjek.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangkapObjek : MonoBehaviour
+{
+    public GameObject bola;
+    public GameObject tangan;
+
+    bool tertangkap = false;
+    Vector3 posisiBola;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posisiBola = bola.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!tertangkap){
+                bola.transform.SetParent(tangan.transform);
+                // bola.transform.localPosition = tangan.transform.localPosition;
+                bola.transform.localPosition = new Vector3(0f, -.61f, .7f);
+                tertangkap = true;
+                bola.GetComponent<Renderer>().material.color = Color.blue;
+            }
+            else if (tertangkap){
+                bola.transform.SetParent(null);
+                bola.transform.localPosition = posisiBola;
+                tertangkap = false;
+                bola.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
