c91f29a54fbd6fbdfe8ae0a750c50a1a63fd476e
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-10-19 09:13:34 +0700

    UTS Minggu 8

---
+++ VR Pertemuan Kedelapan/Assets/Scripts/HalamanUtama.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HalamanUtama : MonoBehaviour
+{
+
+    public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+
+    public void KeluarAplikasi()
+    {
+        Application.Quit();
+    }
+    
+}
