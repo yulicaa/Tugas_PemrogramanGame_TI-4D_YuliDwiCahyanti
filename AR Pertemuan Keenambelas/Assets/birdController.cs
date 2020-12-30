f85b0d9f9a0a72463a808cd4208289f254b32c51
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-12-30 23:35:01 +0700

    Minggu 16

---
+++ AR Pertemuan Keenambelas/Assets/birdController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdController : MonoBehaviour
+{
+    private Transform targetFocus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetFocus = GameObject.FindGameObjectWithTag("target").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = targetFocus.position - this.transform.position;
+        Debug.Log(target.magnitude);
+
+        if (target.magnitude < 1){
+            targetCollider.instance.moveTarget();
+        }
+
+        transform.LookAt(targetFocus.transform);
+        float speed = Random.Range(0.3f, 0.7f);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
