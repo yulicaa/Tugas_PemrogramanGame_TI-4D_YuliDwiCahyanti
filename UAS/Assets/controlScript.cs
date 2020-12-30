a845dd3ce377da594500a7ce7c99afc98350fd8f
Yuli Dwi Cahyanti <43988585+yulicaa@users.noreply.github.com>
2020-12-30 23:27:00 +0700

    UAS

---
+++ UAS/Assets/controlScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class controlScript : MonoBehaviour
+{
+   AudioSource audio;
+   public AudioClip[] clips;
+
+   private IEnumerator introJingle()
+   {
+       yield return new WaitForSeconds(1);
+       playSound(0);
+       StartCoroutine(bunyiBurung());
+   }
+
+   private IEnumerator bunyiBurung()
+   {
+       yield return new WaitForSeconds(1);
+       playSound(1);
+       StartCoroutine(anjingMenyalak());
+   }
+
+   private IEnumerator anjingMenyalak()
+   {
+       yield return new WaitForSeconds(1);
+       playSound(2);
+       StartCoroutine(tembakJingle());
+   }
+
+   private IEnumerator tembakJingle()
+   {
+       yield return new WaitForSeconds(1);
+       playSound(3);
+   }
+
+   public void pindahGame()
+   {
+       SceneManager.LoadScene("main");
+   }
+}
